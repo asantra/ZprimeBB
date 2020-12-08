67bbdd9c505703167c876d244d156717aa067e97
asantra <santra.arka@gmail.com>
2020-12-08 10:34:00 +0200

    UFO file for Zprime

---
+++ VectorZp_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.40
+# Mathematica version: 7.0 for Linux x86 (64-bit) (February 18, 2009)
+# Date: Mon 7 Dec 2020 18:59:48
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
