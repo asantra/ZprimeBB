12b826ad58fc13dce71ba2a9fd08b2e54c7c99e3
asantra <santra.arka@gmail.com>
2020-12-09 17:15:43 +0200

    updated model

---
+++ VectorZp_UFO_v2/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.40
+# Mathematica version: 7.0 for Linux x86 (64-bit) (February 18, 2009)
+# Date: Tue 8 Dec 2020 11:24:47
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
